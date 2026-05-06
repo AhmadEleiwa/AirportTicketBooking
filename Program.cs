1bc38970744efe90e75e923d87f8d658d2fb75fc
= <=>
2026-05-07 02:03:14 +0300

    refactor: add the ConsoleUI and run it in Program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using AirportTicketBooking.Services;
+
+// create services (inject repos)
+var flightService = new FlightService();
+var bookingService = new BookingService();
+
+// create UI
+var ui = new ConsoleUI(flightService, bookingService);
+
+// run app
+ui.Run();
